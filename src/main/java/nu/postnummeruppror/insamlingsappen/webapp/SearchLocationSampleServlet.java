4c915534445d14a3e7fe42d6e4870489ce920606
kalle <kalle@kodapan.se>
2014-09-08 12:22:16 +0200

    allow get via parameters and post via json

--- src/main/java/nu/postnummeruppror/insamlingsappen/webapp/SearchLocationSampleServlet.java
+++ src/main/java/nu/postnummeruppror/insamlingsappen/webapp/SearchLocationSampleServlet.java
@@ -3,9 +3,11 @@ package nu.postnummeruppror.insamlingsappen.webapp;
 import nu.postnummeruppror.insamlingsappen.Insamlingsappen;
 import nu.postnummeruppror.insamlingsappen.domain.LocationSample;
 import nu.postnummeruppror.insamlingsappen.index.LocationSampleIndexFields;
+import org.apache.commons.io.IOUtils;
 import org.apache.lucene.search.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -21,43 +23,21 @@ import java.util.Map;
 public class SearchLocationSampleServlet extends HttpServlet {
 
   @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     try {
 
-      Query coordinateEnvelopeQuery = coordinateEnvelopeQueryFactory(
-          Double.valueOf(request.getParameter("south")),
-          Double.valueOf(request.getParameter("west")),
-          Double.valueOf(request.getParameter("north")),
-          Double.valueOf(request.getParameter("east"))
+      String jsonString = IOUtils.toString(request.getInputStream(), "UTF-8");
+      JSONObject requestJson = new JSONObject(new JSONTokener(jsonString));
+
+      JSONObject json = search(
+          requestJson.getDouble("south"),
+          requestJson.getDouble("west"),
+          requestJson.getDouble("north"),
+          requestJson.getDouble("east"),
+          requestJson.getInt("maximumHits"),
+          requestJson.getString("reference")
       );
-
-      int maximumHits = Integer.valueOf(request.getParameter("maximumHits"));
-
-      JSONObject json = new JSONObject();
-
-      Map<LocationSample, Float> hits = Insamlingsappen.getInstance().getLocationSampleIndex().search(coordinateEnvelopeQuery);
-
-      json.put("success", true);
-      json.put("reference", request.getParameter("reference"));
-      json.put("numberOfHits", hits.size());
-
-      JSONArray hitsJson = new JSONArray();
-      json.put("hits", hitsJson);
-
-      for (LocationSample hit : hits.keySet()) {
-        JSONObject hitJson = new JSONObject();
-        hitJson.put("identity", hit.getIdentity());
-        hitJson.put("postalCode", hit.getPostalCode().getPostalCode());
-        hitJson.put("latitude", hit.getLatitude());
-        hitJson.put("longitude", hit.getLongitude());
-
-        hitsJson.put(hitJson);
-
-        if (hitsJson.length() >= maximumHits) {
-          break;
-        }
-      }
 
       response.setCharacterEncoding("UTF-8");
       response.setContentType("application/json");
@@ -69,6 +49,70 @@ public class SearchLocationSampleServlet extends HttpServlet {
 
   }
 
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    try {
+
+      JSONObject json = search(
+          Double.valueOf(request.getParameter("south")),
+          Double.valueOf(request.getParameter("west")),
+          Double.valueOf(request.getParameter("north")),
+          Double.valueOf(request.getParameter("east")),
+          Integer.valueOf(request.getParameter("maximumHits")),
+          request.getParameter("reference")
+      );
+
+      response.setCharacterEncoding("UTF-8");
+      response.setContentType("application/json");
+      response.getOutputStream().write(json.toString().getBytes("UTF-8"));
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
+
+  public JSONObject search(double south, double west, double north, double east, int maximumHits, String reference) throws Exception {
+
+    Query coordinateEnvelopeQuery = coordinateEnvelopeQueryFactory(south, west, north, east);
+
+    JSONObject json = new JSONObject();
+
+    Map<LocationSample, Float> hits = Insamlingsappen.getInstance().getLocationSampleIndex().search(coordinateEnvelopeQuery);
+
+    json.put("success", true);
+    json.put("reference", reference);
+    json.put("numberOfHits", hits.size());
+
+    JSONArray hitsJson = new JSONArray();
+    json.put("hits", hitsJson);
+
+    for (LocationSample hit : hits.keySet()) {
+      JSONObject hitJson = new JSONObject();
+      hitJson.put("identity", hit.getIdentity());
+
+      hitJson.put("postalCode", hit.getPostalCode().getPostalCode());
+      hitJson.put("postalTown", hit.getPostalTown());
+      hitJson.put("houseNumber", hit.getHouseNumber());
+      hitJson.put("streetName", hit.getStreetName());
+
+      hitJson.put("latitude", hit.getLatitude());
+      hitJson.put("longitude", hit.getLongitude());
+      hitJson.put("accuracy", hit.getAccuracy());
+      hitJson.put("altitude", hit.getAltitude());
+
+      hitsJson.put(hitJson);
+
+      if (hitsJson.length() >= maximumHits) {
+        break;
+      }
+    }
+
+    return json;
+
+  }
 
   public Query coordinateEnvelopeQueryFactory(double south, double west, double north, double east) {
     if (south <= -90d
